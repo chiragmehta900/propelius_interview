29f04c9312fa82224acde0e0867c32b5461e7c89
Chirag Mehta <61863283+chiragmehta900@users.noreply.github.com>
2022-02-26 14:08:50 +0530

    code upload

---
+++ src/Pages/Table.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <div>
+      <h1 class="text-5xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default Table
